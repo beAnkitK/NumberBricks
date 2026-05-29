b1f881c8fde57bbe8178a623cbc1dfc22e757e7f
Ankit Kumar <02ankitkrjsr@gmail.com>
2026-05-29 13:27:57 +0530

    test: add geometry provider tests
    - Add `GeometryProviderTest`
    - Add `TestGeometryProvider` for testing
    - Add `createProps`, `createGridSpec`, and `TEST_ERROR` helpers

--- numberbricks/src/test/kotlin/io/github/beankitk/numberbricks/testing/TestUtils.kt
+++ numberbricks/src/test/kotlin/io/github/beankitk/numberbricks/testing/TestUtils.kt
@@ -1,5 +1,7 @@
 package io.github.beankitk.numberbricks.testing
 
+import io.github.beankitk.numberbricks.core.geometry.GeometryProps
+import io.github.beankitk.numberbricks.core.geometry.GridSpec
 import io.github.beankitk.numberbricks.core.geometry.ProviderKey
 
 /**
@@ -11,3 +13,12 @@ fun <R : Any> createKey(family: ProviderKey<R>? = null): ProviderKey<R> {
         override val family = family ?: this
     }
 }
+
+/** Creates a minimal [GeometryProps] for tests. */
+fun createProps(): GeometryProps = object : GeometryProps {}
+
+/** Creates a [GridSpec] for tests. */
+fun createGridSpec(rows: Int, cols: Int, bricks: Int) = GridSpec(rows, cols, bricks)
+
+val TEST_ERROR: Nothing
+    get() = error("Test Error")
